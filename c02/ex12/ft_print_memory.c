a5c4779cff46c01e69933c57d893b4363e3cd501
biotbs44 <immchaebol@naver.com>
2021-09-25 21:07:44 +0900

    210925

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,8 +6,20 @@
 /*   By: jungyang <jungyang@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 17:53:55 by jungyang          #+#    #+#             */
-/*   Updated: 2021/09/25 18:08:57 by jungyang         ###   ########.fr       */
+/*   Updated: 2021/09/25 21:07:30 by jungyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	*ft_print_memory(void *addr, unsigned int size)
+{
+	unsigned int	char_to_print;
+	unsigned int	offset;
+
+	offset = 0;
+	while (size > 0)
+	{
+		if (size >= 16)
+		{
 
